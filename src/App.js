df40c2acd70f72f3a81bfd9ac717f053906c52ba
FrankAndYang <fffooffff@gmail.com>
2018-10-25 21:31:36 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,51 @@ import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
 import { doTest } from './redux/actions';
+import Header from './Header';
+
 
 class App extends Component {
+  constructor(props) {
+    super (props);
+    this.state ={
+      color: 'black',
+      banner: '',
+      isOpen: false,
+    };
+    this.buttonHandler = this.buttonHandler.bind(this); 
+    this.textHandler = this.textHandler.bind(this);
+    console.log(this.props);
+  }
+
+  buttonHandler(){
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  textHandler(e){
+    this.setState({
+      banner: e.target.value,
+    })
+  }
+
   render() {
+    let myVariable = <h2>Hello 123</h2>;
+    let myBanner;
+    if(this.state.isOpen){
+      myBanner = <Header banner={this.state.banner}/>
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {myVariable}
           </p>
-          <h2>413 Starter Kit </h2>
+          {myBanner}
+          {this.props.test}
+          <input value={this.state.banner} onChange={this.textHandler}/>
+          <button onClick={this.buttonHandler}>Click Me</button>   
         </header>
       </div>
     );
@@ -32,3 +66,4 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(App);
+
